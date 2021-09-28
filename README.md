652fb367d3369c3f4157f3b3d6be62d034bc83e5
mary-sawyer <marysawyer@utexas.edu>
2021-09-28 22:22:56 +0000

    Adding methods to data struct and renaming variables for readability. Adding readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# cryptool
+# cryptool.go
+
+Built with Go 1.16. Uses PBKDF2 with SHA-256 for key generation and AES-256 for encryption.
+
+Usage
+```
+cd cryptool/
+go get golang.org/x/crypto/pbkdf2
+go build cryptool.go
+./cryptool -e plaintext
+./cryptool -d ciphertext
+```
--- cryptool.go
+++ cryptool.go
@@ -9,25 +9,55 @@ import (
 	"encoding/json"
 	"fmt"
 	"golang.org/x/crypto/pbkdf2"
-	"io"
 	"os"
 )
 
-/* SaltLength defines the max length of generated AES salt */
+// SaltLength defines the max length of generated PBKDF2 salt
 var SaltLength int = 16
-/* KeyLength defines the max length of generated AES key */
+
+// KeyLength defines the max length of generated AES key
 var KeyLength int = 32
-/* Iterations defines the number of pbkdf2 iterations the protocol performs */
+
+// Iterations defines the number of pbkdf2 iterations the protocol performs
 var Iterations int = 1000000
-/* File defines the struct holding all data to write/read from disk */
-/* Data is a byte array holding Hex-encoded, AES-encrypted ciphertext */
-/* Salt is a byte array holding Hex-encoded bytes, used by pbkdf2 to recover SHA256 key */
-/* Nonce is a byte array holding Hex-encoded bytes, used by AES to recover plaintext */
-type File struct {
-	Data, Salt, Nonce []byte
+
+// NonceLength defines the max length of generated AES nonce
+var NonceLength int = 12
+
+/* Data defines the struct holding all encryption data to write and read from disk
+   - Ciphertext is a byte array holding Hex-encoded, AES-encrypted file
+   - Salt is a byte array holding Hex-encoded bytes, used by pbkdf2 to recover SHA256 key
+   - Nonce is a byte array holding Hex-encoded bytes, used by AES to recover plaintext */
+type Data struct {
+	Ciphertext, Salt, Nonce []byte
 }
 
-func decrypt(file []byte, key []byte, nonce []byte) []byte {
+func newData(ciphertext []byte, salt []byte, nonce []byte) *Data {
+	return &Data{Ciphertext: ciphertext, Salt: salt, Nonce: nonce}
+}
+
+func (d *Data) encode() {
+	d.Ciphertext = makeHex(d.Ciphertext)
+	d.Salt = makeHex(d.Salt)
+	d.Nonce = makeHex(d.Nonce)
+}
+
+func (d *Data) decode() {
+	d.Ciphertext = unmakeHex(d.Ciphertext)
+	d.Salt = unmakeHex(d.Salt)
+	d.Nonce = unmakeHex(d.Nonce)
+}
+
+func (d *Data) jsonify() []byte {
+	j, err := json.Marshal(d)
+	if err != nil {
+		panic(err.Error())
+	}
+	return j
+}
+
+func decrypt(ciphertext []byte, key []byte, nonce []byte) []byte {
+	// Generate new gcm block cipher using key
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		panic(err.Error())
@@ -38,14 +68,16 @@ func decrypt(file []byte, key []byte, nonce []byte) []byte {
 		panic(err.Error())
 	}
 
-	plaintext, err := gcm.Open(nil, nonce, file, nil)
+	// Decrypt ciphertext using key and nonce
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
 	if err != nil {
 		panic(err.Error())
 	}
 	return plaintext
 }
 
-func encrypt(file []byte, key []byte) ([]byte, []byte) {
+func encrypt(plaintext []byte, key []byte) ([]byte, []byte) {
+	// Generate new gcm block cipher using key
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		panic(err.Error())
@@ -56,26 +88,25 @@ func encrypt(file []byte, key []byte) ([]byte, []byte) {
 		panic(err.Error())
 	}
 
-	// Never use more than 2^32 random nonces with a given key because of the risk of a repeat.
-	nonce := make([]byte, 12)
-	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
-		panic(err.Error())
-	}
+	// Generate cryptographically random nonce
+	nonce := generateBytes(NonceLength)
 
-	return gcm.Seal(nil, nonce, file, nil), nonce
+	// Encrypt plaintext using key and nonce
+	ciphertext := gcm.Seal(nil, nonce, plaintext, nil)
+	return ciphertext, nonce
 }
 
 func generateKey(password string, salt []byte) []byte {
 	return pbkdf2.Key([]byte(password), salt, Iterations, KeyLength, sha256.New)
 }
 
-func generateSalt() []byte {
-	salt := make([]byte, SaltLength)
-	_, err := rand.Read(salt)
+func generateBytes(length int) []byte {
+	b := make([]byte, length)
+	_, err := rand.Read(b)
 	if err != nil {
 		panic(err.Error())
 	}
-	return salt
+	return b
 }
 
 func unmakeHex(in []byte) []byte {
@@ -102,6 +133,8 @@ func printHelp() {
 
 func main() {
 	arguments := os.Args[1:]
+
+	// Check if arguments are ok
 	if len(arguments) < 2 {
 		printHelp()
 		os.Exit(1)
@@ -120,27 +153,36 @@ func main() {
 
 	var key []byte
 
-	if arguments[0] == "-e" {
-		// Generate key given user password
-		var newSalt []byte
-		newSalt = generateSalt()
-		key = generateKey(password, newSalt)
+	if arguments[0] == "-e" { // encryption
+		// Generate salt, then key using password and salt
+		var salt []byte
+		salt = generateBytes(SaltLength)
+		key = generateKey(password, salt)
 
-		fmt.Println("Encrypt", arguments[1], "with pass", password)
-		ciphertext, newNonce := encrypt(file, key)
-		encData := File{
-			Data:  makeHex(ciphertext),
-			Nonce: makeHex(newNonce),
-			Salt:  makeHex(newSalt),
-		}
-		fileOut, _ := json.Marshal(encData)
-		_ = os.WriteFile(arguments[1]+".enc", fileOut, 0644)
-	} else if arguments[0] == "-d" {
-		fmt.Println("Decrypt", arguments[1], "with pass", password)
-		encData := File{}
-		_ = json.Unmarshal([]byte(file), &encData)
-		key = generateKey(password, unmakeHex(encData.Salt))
-		plaintext := decrypt(unmakeHex(encData.Data), key, unmakeHex(encData.Nonce))
+		fmt.Println("Encrypting", arguments[1])
+
+		// Encrypt plaintext file using key
+		ciphertext, nonce := encrypt(file, key)
+
+		// Store encryption data in struct and encode
+		var data Data = *newData(ciphertext, salt, nonce)
+		data.encode()
+
+		// Marshal data to JSON and write to file
+		_ = os.WriteFile(arguments[1]+".enc", data.jsonify(), 0644)
+	} else if arguments[0] == "-d" { // decryption
+		fmt.Println("Decrypting", arguments[1])
+
+		// Unmarshal data from JSON to struct, then decode
+		data := Data{}
+		_ = json.Unmarshal(file, &data)
+		data.decode()
+
+		// Retrieve key, then attempt to decrypt ciphertext
+		key = generateKey(password, data.Salt)
+		plaintext := decrypt(data.Ciphertext, key, data.Nonce)
+
+		// Write plaintext to file
 		_ = os.WriteFile(arguments[1]+".txt", plaintext, 0644)
 	}
 }
